f8b8598ef1bc63c91bf307491ee05bdf641b1f53
Sk-raziya <shaikraziya701@gmail.com>
2025-02-15 11:26:11 +0530

    min_train_platforms.js

--- min_train_platforms.js
+++ min_train_platforms.js
@@ -1,37 +1,28 @@
-function minPlatforms(arrivals, departures) {
-  arrivals.sort((a, b) => a - b);
-  departures.sort((a, b) => a - b);
+//We are given two arrays that represent the arrival and departure times of trains, the
+//task is to find the minimum number of platforms required so that no train waits.
 
-  let platformsNeeded = 1;
-  let result = 1;
-  let i = 1;
-  let j = 0;
 
-  while (i < arrivals.length && j < departures.length) {
-    if (arrivals[i] <= departures[j]) {
-      platformsNeeded++;
-      i++;
-    } else if (arrivals[i] > departures[j]) {
-      platformsNeeded--;
-      j++;
+function minPlatforms(arrival, departure) {
+    arrival.sort((a, b) => a - b);
+    departure.sort((a, b) => a - b);
+
+    let platforms = 0, maxPlatforms = 0;
+    let i = 0, j = 0;
+
+    while (i < arrival.length) {
+        if (arrival[i] < departure[j]) {
+            platforms++;
+            maxPlatforms = Math.max(maxPlatforms, platforms);
+            i++;
+        } else {
+            platforms--;
+            j++;
+        }
     }
-
-    if (platformsNeeded > result) {
-      result = platformsNeeded;
-    }
-  }
-
-  return result;
+    return maxPlatforms;
 }
 
-// Helper function to convert time string to minutes
-function timeToMinutes(time) {
-  const [hours, minutes] = time.split(':').map(Number);
-  return hours * 60 + minutes;
-}
+let arrival = [900, 940, 950, 1100, 1500, 1800];
+let departure = [910, 1200, 1120, 1130, 1900, 2000];
 
-// Test the function
-const arrivals = ['9:00', '9:40', '9:50', '11:00', '15:00', '18:00'].map(timeToMinutes);
-const departures = ['9:10', '12:00', '11:20', '11:30', '19:00', '20:00'].map(timeToMinutes);
-
-console.log(minPlatforms(arrivals, departures));
+console.log(minPlatforms(arrival, departure));
